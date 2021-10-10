0832526d00c445f355406c3ab4ca08350ec7b4bd
Quien Sabe <qs5779@mail.com>
2021-10-09 23:49:43 -0600

    Bedtime.

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Learn more: https://github.com/kennethreitz/setup.py
-
 from setuptools import setup, find_packages
-
 
 with open('README.rst') as f:
     readme = f.read()
@@ -12,14 +9,14 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
+    name='queconverter',
     version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    description='Graphic File Converter',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
-    license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    author='Quien Sabe',
+    author_email='qs5779@mail.com',
+    url='https://github.com/wtfo-guru/queconverter',
+    license='Apache License Version 2.0',
+    packages=find_packages(exclude=('tests', 'docs', 'uc2'))
 )
 
